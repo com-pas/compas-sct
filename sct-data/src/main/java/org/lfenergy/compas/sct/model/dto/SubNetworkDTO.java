05605c2b210d34cca9e74d69a2dac2fd2c441083
SYLLA MOHAMED <mohamed.sylla@rte-france.com>
2021-04-19 14:53:27 +0200

    Add copyright to files
    Signed-off-by: SYLLA MOHAMED <mohamed.sylla@rte-france.com>

--- sct-data/src/main/java/org/lfenergy/compas/sct/model/dto/SubNetworkDTO.java
+++ sct-data/src/main/java/org/lfenergy/compas/sct/model/dto/SubNetworkDTO.java
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2020 RTE FRANCE
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.lfenergy.compas.sct.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
