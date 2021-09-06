5ce20306d9b1a16b2abf287d0de6dc8539d87ba2
Mohamed Sylla <mohamed.sylla@rte-france.com>
2021-09-06 15:25:00 +0200

    remove empty modules, complete documentation ...
    Signed-off-by: Mohamed Sylla <mohamed.sylla@rte-france.com>

---
+++ sct-commons/src/main/java/org/lfenergy/compas/sct/commons/CommonConstants.java
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2021 RTE FRANCE
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.lfenergy.compas.sct.commons;
+
+public class CommonConstants {
+    CommonConstants() {
+        throw new UnsupportedOperationException("CommonConstants class");
+    }
+
+    public static final String XML_DEFAULT_NS_PREFIX = "scl";
+    public static final String XML_DEFAULT_NS_URI = "http://www.iec.ch/61850/2003/SCL";
+    public static final String XML_DEFAULT_XSD_PATH = "classpath:schema/SCL.xsd";
+}
