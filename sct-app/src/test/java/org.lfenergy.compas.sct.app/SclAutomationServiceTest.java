387fccac7fc000121549597f5c28fd645f3c5e61
Aliou DIAITE <aliou.diaite@rte-france.com>
2022-04-21 09:24:27 +0200

    [#65 ==> 95] : correcting history for automation service: add test
    Signed-off-by: Aliou DIAITE <aliou.diaite@rte-france.com>

--- sct-app/src/test/java/org.lfenergy.compas.sct.app/SclAutomationServiceTest.java
+++ sct-app/src/test/java/org.lfenergy.compas.sct.app/SclAutomationServiceTest.java
@@ -12,6 +12,8 @@ import org.lfenergy.compas.sct.commons.exception.ScdException;
 import org.lfenergy.compas.sct.commons.scl.SclRootAdapter;
 import org.lfenergy.compas.sct.commons.testhelpers.SclTestMarshaller;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SclAutomationServiceTest {
@@ -48,8 +50,25 @@ class SclAutomationServiceTest {
         assertEquals(1, expectedSCD.getCurrentElem().getSubstation().size());
     }
 
-
     @Test
+    void createSCD_With_HItems() throws Exception {
+        HeaderDTO.HistoryItem historyItem = new HeaderDTO.HistoryItem();
+        historyItem.setWhat("what");
+        historyItem.setWho("me");
+        historyItem.setWhy("because");
+        HeaderDTO.HistoryItem historyItemBis = new HeaderDTO.HistoryItem();
+        historyItemBis.setWhat("what Bis");
+        historyItemBis.setWho("me bis");
+        historyItemBis.setWhy("because bis");
+        headerDTO.getHistoryItems().addAll(Arrays.asList(historyItem, historyItemBis));
+        SCL ssd = SclTestMarshaller.getSCLFromFile("/scd-substation-import-ssd/ssd.xml");
+        SclRootAdapter expectedSCD = SclAutomationService.createSCD(ssd, headerDTO);
+        assertNotNull(expectedSCD.getCurrentElem().getHeader().getId());
+        assertEquals(1, expectedSCD.getCurrentElem().getHeader().getHistory().getHitem().size());
+        assertEquals("what", expectedSCD.getCurrentElem().getHeader().getHistory().getHitem().get(0).getWhat());
+    }
+
+        @Test
     void createSCD_SSD_Without_Substation() throws Exception {
         SCL ssd = SclTestMarshaller.getSCLFromFile("/scd-substation-import-ssd/ssd_without_substations.xml");
         assertThrows(ScdException.class,
