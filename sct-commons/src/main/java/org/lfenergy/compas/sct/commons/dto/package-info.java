6e298afff0542a32f658bce58297197e615bae2e
Samir Romdhani <samir.romdhani@rte-france.com>
2022-08-25 10:34:43 +0200

    add javadoc WIP
    Signed-off-by: Samir Romdhani <samir.romdhani@rte-france.com>

---
+++ sct-commons/src/main/java/org/lfenergy/compas/sct/commons/dto/package-info.java
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: 2022 RTE FRANCE
+//
+// SPDX-License-Identifier: Apache-2.0
+
+/**
+ *  sct.commons.dto is a group of DTO utils for operating on Scl services.</h3>
+ */
+package org.lfenergy.compas.sct.commons.dto;
