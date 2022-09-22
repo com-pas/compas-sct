a27ffff43a89db8fd48170793a037ecca7006b57
massifben <105049157+massifben@users.noreply.github.com>
2022-09-22 11:25:30 +0200

    [#153] : remove service updateLNodeIEDNames (#162)
    Signed-off-by: massifben <105049157+massifben@users.noreply.github.com>

--- sct-commons/src/main/java/org/lfenergy/compas/sct/commons/scl/sstation/FunctionAdapter.java
+++ sct-commons/src/main/java/org/lfenergy/compas/sct/commons/scl/sstation/FunctionAdapter.java
@@ -3,21 +3,9 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.lfenergy.compas.sct.commons.scl.sstation;
 
-import org.apache.commons.lang3.StringUtils;
-import org.lfenergy.compas.scl2007b4.model.TCompasICDHeader;
 import org.lfenergy.compas.scl2007b4.model.TFunction;
-import org.lfenergy.compas.scl2007b4.model.TLNode;
-import org.lfenergy.compas.scl2007b4.model.TPrivate;
-import org.lfenergy.compas.sct.commons.exception.ScdException;
-import org.lfenergy.compas.sct.commons.scl.PrivateService;
 import org.lfenergy.compas.sct.commons.scl.SclElementAdapter;
 import org.lfenergy.compas.sct.commons.util.Utils;
-
-import java.util.List;
-import java.util.ListIterator;
-import java.util.stream.Collectors;
-
-import static org.lfenergy.compas.sct.commons.util.PrivateEnum.COMPAS_ICDHEADER;
 
 /**
  * A representation of the model object
@@ -30,9 +18,6 @@ import static org.lfenergy.compas.sct.commons.util.PrivateEnum.COMPAS_ICDHEADER;
  *    <ul>
  *      <li>{@link FunctionAdapter#addPrivate <em>Add <b>TPrivate </b> under this object</em>}</li>
  *      <li>{@link FunctionAdapter#elementXPath <em>Returns the <b>XPATH </b> for this object</em>}</li>
- *      <li>{@link FunctionAdapter#distributeIcdHeaders(TLNode, List) <em>Add <b>TCompasICDHeader </b> Private in each <b>TLNode </b> reference object</em>}</li>
- *      <li>{@link FunctionAdapter#updateLNodeIedNames() <em>Update the value of <b>LNode@iedName </b> attribute corresponding to the value of <b>ICDHeader@IEDName </b> attribute </em> }</li>
- *      <li>{@link FunctionAdapter#setLNodeIedName(TLNode, String) <em>Update the value of <b>LNode@iedName </b> attribute</em>}</li>
  *    </ul>
  *  </ol>
  *   <p>
@@ -74,65 +59,4 @@ public class FunctionAdapter extends SclElementAdapter<BayAdapter, TFunction> {
         return String.format("Function[%s]", Utils.xpathAttributeFilter("name", currentElem.isSetName() ? currentElem.getName() : null));
     }
 
-    /**
-     * Updates LNode IED name in current Function
-     * <ul>
-     *     <li>If LNode contents one Private Compas-ICDHEADER so LNode name is update with IED name from the Private</li>
-     *     <li>If LNode contents more than one Private Compas-ICDHEADER so new LNodes are created for each Private Compas-ICDHEADER
-     *         and naming them with the Private IED name</li>
-     * </ul>
-     * @throws ScdException throws when no Private Compas-ICDHEADER found on at least one LNode of current Function
-     */
-    public void updateLNodeIedNames() throws ScdException {
-        if (!currentElem.isSetLNode()) {
-            return;
-        }
-        // use ListIterator to add & remove items in place
-        ListIterator<TLNode> lNodeIterator = currentElem.getLNode().listIterator();
-        while (lNodeIterator.hasNext()) {
-            TLNode lNode = lNodeIterator.next();
-            List<TCompasICDHeader> icdHeaders = PrivateService.getCompasPrivates(lNode, TCompasICDHeader.class);
-            if (icdHeaders.isEmpty()){
-                throw new ScdException(getXPath() + " doesn't contain any Private/compas:ICDHeader");
-            }
-            if (icdHeaders.size() == 1) {
-                setLNodeIedName(lNode, icdHeaders.get(0).getIEDName());
-            } else {
-                lNodeIterator.remove();
-                PrivateService.removePrivates(lNode, COMPAS_ICDHEADER);
-                List<TLNode> newLNodes = distributeIcdHeaders(lNode, icdHeaders);
-                newLNodes.forEach(lNodeIterator::add);
-            }
-        }
-    }
-
-    /**
-     * Splits given LNode by Private Compas-ICDHEADER in list of new LNodes which have one and only one Private Compas-ICDHEADER for each of them
-     * @param lNode LNode to split
-     * @param compasICDHeaders list of Compas-ICDHEADER for each LNode should be created
-     * @return list of <em>TLNode</em> object
-     */
-    private List<TLNode> distributeIcdHeaders(TLNode lNode, List<TCompasICDHeader> compasICDHeaders) {
-        LNodeAdapter lNodeAdapter = new LNodeAdapter(null, lNode);
-        return compasICDHeaders.stream().map(compasICDHeader -> {
-            TPrivate icdHeaderPrivate = PrivateService.createPrivate(compasICDHeader);
-            TLNode newLNode = lNodeAdapter.deepCopy();
-            newLNode.getPrivate().add(icdHeaderPrivate);
-            setLNodeIedName(newLNode, compasICDHeader.getIEDName());
-            return newLNode;
-        }).collect(Collectors.toList());
-    }
-
-    /**
-     * Sets LNode's IEDName from given name
-     * @param lNode LNode for which IEDName should be updated
-     * @param privateIedName new name of LNode IEDName
-     */
-    private void setLNodeIedName(TLNode lNode, String privateIedName) {
-        if (StringUtils.isBlank(privateIedName)){
-            LNodeAdapter lNodeAdapter = new LNodeAdapter(null, lNode);
-            throw new ScdException(getXPath() + lNodeAdapter.getXPath() + "/Private/compas:ICDHeader/@IEDName is missing or is blank");
-        }
-        lNode.setIedName(privateIedName);
-    }
 }
--- sct-commons/src/test/java/org/lfenergy/compas/sct/commons/scl/sstation/FunctionAdapterTest.java
+++ sct-commons/src/test/java/org/lfenergy/compas/sct/commons/scl/sstation/FunctionAdapterTest.java
@@ -7,14 +7,9 @@ package org.lfenergy.compas.sct.commons.scl.sstation;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.lfenergy.compas.scl2007b4.model.TBay;
-import org.lfenergy.compas.scl2007b4.model.TCompasICDHeader;
 import org.lfenergy.compas.scl2007b4.model.TFunction;
-import org.lfenergy.compas.scl2007b4.model.TLNode;
-import org.lfenergy.compas.sct.commons.exception.ScdException;
-import org.lfenergy.compas.sct.commons.scl.PrivateService;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class FunctionAdapterTest {
 
@@ -56,54 +51,6 @@ class FunctionAdapterTest {
         String result = functionAdapter.elementXPath();
         // Then
         assertThat(result).isEqualTo("Function[@name=\"functionName\"]");
-    }
-
-    @Test
-    void updateLNodeIedNames_when_one_private_should_succeed() {
-        // Given
-        TLNode tlNode = new TLNode();
-        TCompasICDHeader tCompasICDHeader = new TCompasICDHeader();
-        tCompasICDHeader.setIEDName("iedName1");
-        tlNode.getPrivate().add(PrivateService.createPrivate(tCompasICDHeader));
-        functionAdapter.getCurrentElem().getLNode().add(tlNode);
-        // When
-        functionAdapter.updateLNodeIedNames();
-        // Then
-        assertThat(functionAdapter.getCurrentElem().getLNode())
-            .hasSize(1)
-            .map(TLNode::getIedName).containsExactly("iedName1");
-    }
-
-    @Test
-    void updateLNodeIedNames_when_multiples_privates_should_succeed() {
-        // Given
-        TLNode tlNode = new TLNode();
-        TCompasICDHeader tCompasICDHeader1 = new TCompasICDHeader();
-        tCompasICDHeader1.setIEDName("iedName1");
-        TCompasICDHeader tCompasICDHeader2 = new TCompasICDHeader();
-        tCompasICDHeader2.setIEDName("iedName2");
-        TCompasICDHeader tCompasICDHeader3 = new TCompasICDHeader();
-        tCompasICDHeader3.setIEDName("iedName3");
-        tlNode.getPrivate().add(PrivateService.createPrivate(tCompasICDHeader1));
-        tlNode.getPrivate().add(PrivateService.createPrivate(tCompasICDHeader2));
-        tlNode.getPrivate().add(PrivateService.createPrivate(tCompasICDHeader3));
-        functionAdapter.getCurrentElem().getLNode().add(tlNode);
-        // When
-        functionAdapter.updateLNodeIedNames();
-        // Then
-        assertThat(functionAdapter.getCurrentElem().getLNode())
-            .hasSize(3)
-            .map(TLNode::getIedName)
-            .containsExactly("iedName1", "iedName2", "iedName3");
-    }
-
-    @Test
-    void updateLNodeIedNames_when_no_private_should_throw_exception() {
-        // Given
-        TLNode tlNode = new TLNode();
-        functionAdapter.getCurrentElem().getLNode().add(tlNode);
-        // When & Then
-        assertThatThrownBy(() -> functionAdapter.updateLNodeIedNames()).isInstanceOf(ScdException.class);
     }
 
 }
