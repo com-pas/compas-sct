06a49b2fb72c89943c8060259a3af9f5bfc8286c
Aliou DIAITE <aliou.diaite@rte-france.com>
2022-10-24 16:26:30 +0200

    Fix Control Blocks matching RxtRefs (#180)
    * fix(175): gets all control blocks source for DataSet for which at least FCDA match ExtRef
    
    Signed-off-by: Aliou DIAITE <aliou.diaite@rte-france.com>
    Signed-off-by: SABATIER Philippe Ext <philippe.sabatier@rte-france.com>
    Co-authored-by: psabatierrte <98765347+psabatierrte@users.noreply.github.com>
    Signed-off-by: Aliou DIAITE <aliou.diaite@rte-france.com>

--- sct-commons/src/test/java/org/lfenergy/compas/sct/commons/dto/ExtRefInfoTest.java
+++ sct-commons/src/test/java/org/lfenergy/compas/sct/commons/dto/ExtRefInfoTest.java
@@ -5,10 +5,16 @@
 package org.lfenergy.compas.sct.commons.dto;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.lfenergy.compas.scl2007b4.model.TExtRef;
 import org.lfenergy.compas.scl2007b4.model.TFCDA;
 import org.lfenergy.compas.scl2007b4.model.TServiceType;
 
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ExtRefInfoTest {
@@ -92,4 +98,78 @@ class ExtRefInfoTest {
         return extRefInfo;
     }
 
+    @Test
+    void checkMatchingFCDA_shouldreturnTrue_whenContentMatch() {
+        //Given
+        ExtRefInfo extRefInfo = new ExtRefInfo();
+        extRefInfo.setBindingInfo(DTO.createExtRefBindingInfo_Remote());
+        extRefInfo.setSignalInfo(DTO.createExtRefSignalInfo());
+
+        TFCDA tfcda = new TFCDA();
+        tfcda.setLdInst(DTO.createExtRefBindingInfo_Remote().getLdInst());
+        tfcda.getLnClass().add(DTO.createExtRefBindingInfo_Remote().getLnClass());
+        tfcda.setLnInst(DTO.createExtRefBindingInfo_Remote().getLnInst());
+        tfcda.setPrefix(DTO.createExtRefBindingInfo_Remote().getPrefix());
+        tfcda.setDoName(DTO.createExtRefSignalInfo().getPDO());
+        tfcda.setDaName(DTO.createExtRefSignalInfo().getPDA());
+
+        //When Then
+        assertThat(extRefInfo.checkMatchingFCDA(tfcda)).isTrue();
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("provideIncompleteExtRefInfo")
+    void checkMatchingFCDA(String testCase, TFCDA fcda, ExtRefBindingInfo bindingInfo, ExtRefSignalInfo signalInfo) {
+        //Given
+        ExtRefInfo extRefInfo = new ExtRefInfo();
+        extRefInfo.setBindingInfo(bindingInfo);
+        extRefInfo.setSignalInfo(signalInfo);
+        //When Then
+        assertThat(extRefInfo.checkMatchingFCDA(fcda)).isFalse();
+    }
+
+
+    private static Stream<Arguments> provideIncompleteExtRefInfo() {
+
+        ExtRefBindingInfo bindingInfo = DTO.createExtRefBindingInfo_Remote();
+        ExtRefSignalInfo signalInfo = DTO.createExtRefSignalInfo();
+
+        ExtRefBindingInfo extRefBindingInfoLDInstNotMatch = DTO.createExtRefBindingInfo_Remote();
+        extRefBindingInfoLDInstNotMatch.setLdInst("ldInst");
+
+        ExtRefBindingInfo extRefBindingInfoLNClassNotMatch = DTO.createExtRefBindingInfo_Remote();
+        extRefBindingInfoLNClassNotMatch.setLnClass("lnClass");
+
+        ExtRefBindingInfo xtRefBindingInfoLNInstNotMatch = DTO.createExtRefBindingInfo_Remote();
+        xtRefBindingInfoLNInstNotMatch.setLnInst("5");
+
+        ExtRefBindingInfo ExtRefBindingInfoPrefixNotMatch = DTO.createExtRefBindingInfo_Remote();
+        ExtRefBindingInfoPrefixNotMatch.setPrefix("Pref");
+
+        ExtRefSignalInfo extRefSignalInfoDOnotMatch = DTO.createExtRefSignalInfo();
+        extRefSignalInfoDOnotMatch.setPDO("do");
+
+        ExtRefSignalInfo extRefSignalInfoDAnotMatch = DTO.createExtRefSignalInfo();
+        extRefSignalInfoDAnotMatch.setPDA("da");
+
+        TFCDA tfcda = new TFCDA();
+        tfcda.setLdInst(DTO.createExtRefBindingInfo_Remote().getLdInst());
+        tfcda.getLnClass().add(DTO.createExtRefBindingInfo_Remote().getLnClass());
+        tfcda.setLnInst(DTO.createExtRefBindingInfo_Remote().getLnInst());
+        tfcda.setPrefix(DTO.createExtRefBindingInfo_Remote().getPrefix());
+        tfcda.setDoName(DTO.createExtRefSignalInfo().getPDO());
+        tfcda.setDaName(DTO.createExtRefSignalInfo().getPDA());
+
+        return Stream.of(
+                Arguments.of("return false when binding and signal are null ", tfcda, null, null),
+                Arguments.of("return false when only signal is null ", tfcda, bindingInfo, null),
+                Arguments.of("return false when only binding is null ", tfcda, null, signalInfo),
+                Arguments.of("return false when ldInst not match ", tfcda, extRefBindingInfoLDInstNotMatch, signalInfo),
+                Arguments.of("return false when lnClass not match ", tfcda, extRefBindingInfoLNClassNotMatch, signalInfo),
+                Arguments.of("return false when lNInst not match ", tfcda, xtRefBindingInfoLNInstNotMatch, signalInfo),
+                Arguments.of("return false when prefix not match ", tfcda, ExtRefBindingInfoPrefixNotMatch, signalInfo),
+                Arguments.of("return false when pDO not match ",tfcda, bindingInfo, extRefSignalInfoDOnotMatch),
+                Arguments.of("return false when pDA not match ",tfcda, bindingInfo, extRefSignalInfoDAnotMatch)
+        );
+    }
 }
--- sct-commons/src/test/java/org/lfenergy/compas/sct/commons/dto/FCDAInfoTest.java
+++ sct-commons/src/test/java/org/lfenergy/compas/sct/commons/dto/FCDAInfoTest.java
@@ -5,10 +5,17 @@
 package org.lfenergy.compas.sct.commons.dto;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.lfenergy.compas.scl2007b4.model.TFCDA;
 import org.lfenergy.compas.scl2007b4.model.TFCEnum;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class FCDAInfoTest {
 
@@ -49,5 +56,69 @@ class FCDAInfoTest {
         assertEquals(tfcda1.getFc(),fcdaInfo1.getFc());
 
         assertTrue(fcdaInfo1.isValid());
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("providerFCDAInfoObjects")
+    void checkFCDACompatibilitiesForBinding(String testCase, FCDAInfo fcdaInfo, FCDAInfo expectedFcdaInfo) {
+        assertThat(fcdaInfo.checkFCDACompatibilitiesForBinding(expectedFcdaInfo)).isFalse();
+    }
+
+    @Test
+    void checkFCDACompatibilitiesForBinding_shouldReturnTrue_whensameContent() {
+        //Given
+        TFCDA tfcda = new TFCDA();
+        tfcda.setLdInst(DTO.createExtRefBindingInfo_Remote().getLdInst());
+        tfcda.getLnClass().add(DTO.createExtRefBindingInfo_Remote().getLnClass());
+        tfcda.setLnInst(DTO.createExtRefBindingInfo_Remote().getLnInst());
+        tfcda.setDoName(DTO.createExtRefSignalInfo().getPDO());
+        tfcda.setDaName(DTO.createExtRefSignalInfo().getPDA());
+
+        FCDAInfo fcdaInfo = new FCDAInfo(tfcda);
+        FCDAInfo expectedFcdaInfo = new FCDAInfo(tfcda);
+        //When Then
+        assertThat(fcdaInfo.checkFCDACompatibilitiesForBinding(expectedFcdaInfo)).isTrue();
+    }
+
+    private static Stream<Arguments> providerFCDAInfoObjects(){
+
+        TFCDA tfcda = new TFCDA();
+        tfcda.setLdInst(DTO.createExtRefBindingInfo_Remote().getLdInst());
+        tfcda.getLnClass().add(DTO.createExtRefBindingInfo_Remote().getLnClass());
+        tfcda.setLnInst(DTO.createExtRefBindingInfo_Remote().getLnInst());
+        tfcda.setDoName(DTO.createExtRefSignalInfo().getPDO());
+        tfcda.setDaName(DTO.createExtRefSignalInfo().getPDA());
+
+        FCDAInfo fcdaInfo = new FCDAInfo(tfcda);
+
+        FCDAInfo expectedFcdaInfo = new FCDAInfo(tfcda);
+
+        FCDAInfo expectedLDInstNotMatch = new FCDAInfo(tfcda);
+        expectedLDInstNotMatch.setLdInst("LDCMDDJ");
+
+        FCDAInfo expectedLNClassNotMatch = new FCDAInfo(tfcda);
+        expectedLNClassNotMatch.setLnClass("CSWI");
+
+        FCDAInfo expectedLNInstNotMatch = new FCDAInfo(tfcda);
+        expectedLNInstNotMatch.setLnInst("5");
+
+        FCDAInfo expectedPrefixNotMatch = new FCDAInfo(tfcda);
+        expectedPrefixNotMatch.setPrefix("Prefix");
+
+        FCDAInfo expectedDONotMatch = new FCDAInfo(tfcda);
+        expectedDONotMatch.setDoName(new DoTypeName("do.do1"));
+
+        FCDAInfo expectedDANotMatch = new FCDAInfo(tfcda);
+        expectedDANotMatch.setDaName(new DaTypeName("da.bda.bda1"));
+
+        return Stream.of(
+                Arguments.of("return false when LdInst not match ", fcdaInfo, expectedLDInstNotMatch),
+                Arguments.of("return false when lnClass not match ", fcdaInfo, expectedLNClassNotMatch),
+                Arguments.of("return false when lnInst not match ", fcdaInfo, expectedLNInstNotMatch),
+                Arguments.of("return false when Prefix not match ", fcdaInfo, expectedPrefixNotMatch),
+                Arguments.of("return false when DO not match ", fcdaInfo, expectedDONotMatch),
+                Arguments.of("return false when DO not match ", fcdaInfo, expectedDANotMatch)
+
+        );
     }
 }
