edee7ac832a4cc79e5450364121336601ee38036
gleizesDor <115622893+gleizesDor@users.noreply.github.com>
2023-06-07 17:44:45 +0200

    refactor(#249): Modify extractCompasPrivate method to respect good developpement practices
    Signed-off-by: gleizesDor <115622893+gleizesDor@users.noreply.github.com>

---
+++ sct-commons/src/main/java/org/lfenergy/compas/sct/commons/scl/icd/IcdHeader.java
@@ -0,0 +1,97 @@
+// SPDX-FileCopyrightText: 2021 RTE FRANCE
+//
+// SPDX-License-Identifier: Apache-2.0
+package org.lfenergy.compas.sct.commons.scl.icd;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.lfenergy.compas.scl2007b4.model.TCompasICDHeader;
+import org.lfenergy.compas.scl2007b4.model.TCompasIEDRedundancy;
+import org.lfenergy.compas.scl2007b4.model.TCompasIEDType;
+import org.lfenergy.compas.sct.commons.exception.ScdException;
+
+import java.math.BigInteger;
+import java.util.Optional;
+
+import static org.lfenergy.compas.sct.commons.util.CommonConstants.*;
+
+/**
+ * Object representing '<compas:ICDHeader' , but with utility methods to manipulate the business
+ * Do not include "currentElements" and so on inside this class
+ */
+@Getter
+@EqualsAndHashCode
+public class IcdHeader {
+
+    @EqualsAndHashCode.Exclude
+    private final String content;
+    private final String icdSystemVersionUUID;
+    private final TCompasIEDType iedType;
+    @EqualsAndHashCode.Exclude
+    private final BigInteger iedSubstationinstance;
+    @EqualsAndHashCode.Exclude
+    private final BigInteger iedSystemVersioninstance;
+    @EqualsAndHashCode.Exclude
+    private final String iedName;
+    private final String vendorName;
+    private final String iedModel;
+    private final TCompasIEDRedundancy iedRedundancy;
+    @EqualsAndHashCode.Exclude
+    private final String bayLabel;
+    private final String hwRev;
+    private final String swRev;
+    private final String headerId;
+    private final String headerVersion;
+    private final String headerRevision;
+
+    public IcdHeader(TCompasICDHeader compasICDHeader) {
+        this.content = compasICDHeader.getContent();
+        this.icdSystemVersionUUID = compasICDHeader.getICDSystemVersionUUID();
+        this.iedType = compasICDHeader.getIEDType();
+        this.iedSubstationinstance = compasICDHeader.getIEDSubstationinstance();
+        this.iedSystemVersioninstance = compasICDHeader.getIEDSystemVersioninstance();
+        this.iedName = compasICDHeader.getIEDName();
+        this.vendorName = compasICDHeader.getVendorName();
+        this.iedModel = compasICDHeader.getIEDmodel();
+        this.iedRedundancy = compasICDHeader.getIEDredundancy();
+        this.bayLabel = compasICDHeader.getBayLabel();
+        this.hwRev = compasICDHeader.getHwRev();
+        this.swRev = compasICDHeader.getSwRev();
+        this.headerId = compasICDHeader.getHeaderId();
+        this.headerVersion = compasICDHeader.getHeaderVersion();
+        this.headerRevision = compasICDHeader.getHeaderRevision();
+    }
+
+    public TCompasICDHeader toTCompasICDHeader() {
+        TCompasICDHeader tCompasICDHeader = new TCompasICDHeader();
+        tCompasICDHeader.setContent(content);
+        tCompasICDHeader.setICDSystemVersionUUID(icdSystemVersionUUID);
+        tCompasICDHeader.setIEDType(iedType);
+        tCompasICDHeader.setIEDSubstationinstance(iedSubstationinstance);
+        tCompasICDHeader.setIEDSystemVersioninstance(iedSystemVersioninstance);
+        tCompasICDHeader.setIEDName(iedName);
+        tCompasICDHeader.setVendorName(vendorName);
+        tCompasICDHeader.setIEDmodel(iedModel);
+        tCompasICDHeader.setIEDredundancy(iedRedundancy);
+        tCompasICDHeader.setBayLabel(bayLabel);
+        tCompasICDHeader.setHwRev(hwRev);
+        tCompasICDHeader.setSwRev(swRev);
+        tCompasICDHeader.setHeaderId(headerId);
+        tCompasICDHeader.setHeaderVersion(headerVersion);
+        tCompasICDHeader.setHeaderRevision(headerRevision);
+        return tCompasICDHeader;
+    }
+
+    public String getIcdSystemVersionUUID() {
+        return Optional.of(icdSystemVersionUUID)
+                .orElseThrow(() -> new ScdException(ICD_SYSTEM_VERSION_UUID + " is not present in COMPAS-ICDHeader in LNode"));
+    }
+
+    @Override
+    public String toString() {
+        return HEADER_ID + " = " + headerId + ", " +
+                HEADER_VERSION + " = " + headerVersion + ", " +
+                HEADER_REVISION + " = " + headerRevision +
+                " and " + ICD_SYSTEM_VERSION_UUID + " = " + icdSystemVersionUUID;
+    }
+}
---
+++ sct-commons/src/test/java/org/lfenergy/compas/sct/commons/scl/icd/IcdHeaderTest.java
@@ -0,0 +1,170 @@
+// SPDX-FileCopyrightText: 2021 RTE FRANCE
+//
+// SPDX-License-Identifier: Apache-2.0
+package org.lfenergy.compas.sct.commons.scl.icd;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.lfenergy.compas.scl2007b4.model.TCompasICDHeader;
+import org.lfenergy.compas.scl2007b4.model.TCompasIEDRedundancy;
+import org.lfenergy.compas.scl2007b4.model.TCompasIEDType;
+
+import java.math.BigInteger;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class IcdHeaderTest {
+
+    @Test
+    void toTCompasICDHeader() {
+        //Given
+        IcdHeader icdHeader = new IcdHeader(createHeader());
+        //When
+        TCompasICDHeader tCompasICDHeader = icdHeader.toTCompasICDHeader();
+        //Then
+        assertThat(tCompasICDHeader)
+                .usingRecursiveComparison()
+                .isEqualTo(createHeader());
+    }
+
+    @Test
+    void getIcdSystemVersionUUID() {
+        //Given
+        IcdHeader icdHeader = new IcdHeader(createHeader());
+        //When
+        String icdSystemVersionUUID = icdHeader.getIcdSystemVersionUUID();
+        //Then
+        assertThat(icdSystemVersionUUID).isEqualTo("icdSystemVersionUUID");
+    }
+
+    @Test
+    void testToString() {
+        //Given
+        IcdHeader icdHeader = new IcdHeader(createHeader());
+        //When
+        String icdHeaderString = icdHeader.toString();
+        //Then
+        assertThat(icdHeaderString).isEqualTo("headerId = headerId, headerVersion = headerVersion, headerRevision = headerRevision and ICDSystemVersionUUID = icdSystemVersionUUID");
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideIcdHeaderForEquals")
+    void testEquals(IcdHeader icdHeader2) {
+        //Given
+        IcdHeader icdHeader = new IcdHeader(createHeader());
+
+        //When
+        //Then
+        assertThat(icdHeader).isEqualTo(icdHeader2);
+    }
+
+    private static Stream<Arguments> provideIcdHeaderForEquals() {
+        TCompasICDHeader tCompasICDHeaderModifiedContent = createHeader();
+        tCompasICDHeaderModifiedContent.setContent("patate");
+        TCompasICDHeader tCompasICDHeaderModifiedIedSubstationinstance = createHeader();
+        tCompasICDHeaderModifiedIedSubstationinstance.setIEDSubstationinstance(BigInteger.TEN);
+        TCompasICDHeader tCompasICDHeaderModifiediedSystemVersioninstance = createHeader();
+        tCompasICDHeaderModifiediedSystemVersioninstance.setIEDSystemVersioninstance(BigInteger.TEN);
+        TCompasICDHeader tCompasICDHeaderModifiediedName = createHeader();
+        tCompasICDHeaderModifiediedName.setIEDName("patate");
+        TCompasICDHeader tCompasICDHeaderModifiedbayLabel = createHeader();
+        tCompasICDHeaderModifiedbayLabel.setBayLabel("patate");
+        return Stream.of(
+                Arguments.of(new IcdHeader(tCompasICDHeaderModifiedContent)),
+                Arguments.of(new IcdHeader(tCompasICDHeaderModifiedIedSubstationinstance)),
+                Arguments.of(new IcdHeader(tCompasICDHeaderModifiediedSystemVersioninstance)),
+                Arguments.of(new IcdHeader(tCompasICDHeaderModifiediedName)),
+                Arguments.of(new IcdHeader(tCompasICDHeaderModifiedbayLabel))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideIcdHeaderForNotEquals")
+    void testEquals_should_not_equals(IcdHeader icdHeader2) {
+        //Given
+        IcdHeader icdHeader = new IcdHeader(createHeader());
+        //When
+        //Then
+        assertThat(icdHeader).isNotEqualTo(icdHeader2);
+    }
+
+    private static Stream<Arguments> provideIcdHeaderForNotEquals() {
+        TCompasICDHeader tCompasICDHeaderModifiedicdSystemVersionUUID = createHeader();
+        tCompasICDHeaderModifiedicdSystemVersionUUID.setICDSystemVersionUUID("patate");
+        TCompasICDHeader tCompasICDHeaderModifiediedType = createHeader();
+        tCompasICDHeaderModifiediedType.setIEDType(TCompasIEDType.AUT);
+        TCompasICDHeader tCompasICDHeaderModifiedvendorName = createHeader();
+        tCompasICDHeaderModifiedvendorName.setVendorName("patate");
+        TCompasICDHeader tCompasICDHeaderModifiediedModel = createHeader();
+        tCompasICDHeaderModifiediedModel.setIEDmodel("patate");
+        TCompasICDHeader tCompasICDHeaderModifiediedRedundancy = createHeader();
+        tCompasICDHeaderModifiediedRedundancy.setIEDredundancy(TCompasIEDRedundancy.B);
+        TCompasICDHeader tCompasICDHeaderModifiedhwRev = createHeader();
+        tCompasICDHeaderModifiedhwRev.setHwRev("patate");
+        TCompasICDHeader tCompasICDHeaderModifiedswRev = createHeader();
+        tCompasICDHeaderModifiedswRev.setSwRev("patate");
+        TCompasICDHeader tCompasICDHeaderModifiedheaderId = createHeader();
+        tCompasICDHeaderModifiedheaderId.setHeaderId("patate");
+        TCompasICDHeader tCompasICDHeaderModifiedheaderVersion = createHeader();
+        tCompasICDHeaderModifiedheaderVersion.setHeaderVersion("patate");
+        TCompasICDHeader tCompasICDHeaderModifiedheaderRevision = createHeader();
+        tCompasICDHeaderModifiedheaderRevision.setHeaderRevision("patate");
+        return Stream.of(
+                Arguments.of(new IcdHeader(tCompasICDHeaderModifiedicdSystemVersionUUID)),
+                Arguments.of(new IcdHeader(tCompasICDHeaderModifiediedType)),
+                Arguments.of(new IcdHeader(tCompasICDHeaderModifiedvendorName)),
+                Arguments.of(new IcdHeader(tCompasICDHeaderModifiediedModel)),
+                Arguments.of(new IcdHeader(tCompasICDHeaderModifiediedRedundancy)),
+                Arguments.of(new IcdHeader(tCompasICDHeaderModifiedhwRev)),
+                Arguments.of(new IcdHeader(tCompasICDHeaderModifiedswRev)),
+                Arguments.of(new IcdHeader(tCompasICDHeaderModifiedheaderId)),
+                Arguments.of(new IcdHeader(tCompasICDHeaderModifiedheaderVersion)),
+                Arguments.of(new IcdHeader(tCompasICDHeaderModifiedheaderRevision))
+        );
+    }
+
+    @Test
+    void testHashCode() {
+        //Given
+        IcdHeader icdHeader = new IcdHeader(createHeader());
+        IcdHeader icdHeader2 = new IcdHeader(createHeader());
+        //When
+        //Then
+        assertThat(icdHeader).hasSameHashCodeAs(icdHeader2);
+    }
+
+    @Test
+    void testHashCode_should_not_equals() {
+        //Given
+        IcdHeader icdHeader = new IcdHeader(createHeader());
+        TCompasICDHeader header2 = createHeader();
+        header2.setVendorName("patate");
+        IcdHeader icdHeader2 = new IcdHeader(header2);
+        //When
+        //Then
+        assertThat(icdHeader).doesNotHaveSameHashCodeAs(icdHeader2);
+    }
+
+    private static TCompasICDHeader createHeader() {
+        TCompasICDHeader tCompasICDHeader = new TCompasICDHeader();
+        tCompasICDHeader.setContent("content");
+        tCompasICDHeader.setICDSystemVersionUUID("icdSystemVersionUUID");
+        tCompasICDHeader.setIEDType(TCompasIEDType.SCU);
+        tCompasICDHeader.setIEDSubstationinstance(BigInteger.ONE);
+        tCompasICDHeader.setIEDSystemVersioninstance(BigInteger.ONE);
+        tCompasICDHeader.setIEDName("iedName");
+        tCompasICDHeader.setVendorName("vendorName");
+        tCompasICDHeader.setIEDmodel("iedModel");
+        tCompasICDHeader.setIEDredundancy(TCompasIEDRedundancy.A);
+        tCompasICDHeader.setBayLabel("bayLabel");
+        tCompasICDHeader.setHwRev("hwRev");
+        tCompasICDHeader.setSwRev("swRev");
+        tCompasICDHeader.setHeaderId("headerId");
+        tCompasICDHeader.setHeaderVersion("headerVersion");
+        tCompasICDHeader.setHeaderRevision("headerRevision");
+        return tCompasICDHeader;
+    }
+}
