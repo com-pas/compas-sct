f3d33926642e6a9a1df2168ec0f346c48d6fbf2e
massifben <105049157+massifben@users.noreply.github.com>
2023-06-09 13:04:29 +0200

    fix(#287): fix invalid prefix for Compas Privates when marshalling
    Signed-off-by: massifben <105049157+massifben@users.noreply.github.com>

---
+++ sct-commons/src/test/java/org/lfenergy/compas/scl2007b4/model/NamespaceConfigurationTest.java
@@ -0,0 +1,39 @@
+// SPDX-FileCopyrightText: 2023 RTE FRANCE
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.lfenergy.compas.scl2007b4.model;
+
+import org.junit.jupiter.api.Test;
+import org.lfenergy.compas.sct.commons.scl.PrivateService;
+import org.lfenergy.compas.sct.commons.testhelpers.MarshallerWrapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NamespaceConfigurationTest {
+
+    @Test
+    void marshalling_SCL_and_Compas_Privates_should_set_correct_prefix() {
+        // Given
+        SCL scl = createValidScl();
+        TPrivate aCompasPrivate = PrivateService.createPrivate(TCompasSclFileType.SCD);
+        scl.getPrivate().add(aCompasPrivate);
+        // When
+        String result = MarshallerWrapper.marshall(scl);
+        // Then
+        assertThat(result)
+                .containsPattern("(?s)<SCL[^>]* xmlns=\"http://www\\.iec\\.ch/61850/2003/SCL\"")
+                .containsPattern("(?s)<SCL[^>]* xmlns:compas=\"https://www\\.lfenergy\\.org/compas/extension/v1\"");
+    }
+
+    private static SCL createValidScl() {
+        SCL scl = new SCL();
+        scl.setVersion("2007");
+        scl.setRevision("B");
+        scl.setRelease((short) 4);
+        THeader tHeader = new THeader();
+        tHeader.setId("headerId");
+        scl.setHeader(tHeader);
+        return scl;
+    }
+}
