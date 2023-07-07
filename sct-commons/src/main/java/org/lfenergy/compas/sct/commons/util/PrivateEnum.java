bc0ee92677d372af71a1fe19cc39b4a5d87e82a5
massifben <105049157+massifben@users.noreply.github.com>
2023-07-07 14:39:18 +0200

    feat(#293): add CompasTopo to PrivateEnum
    Signed-off-by: massifben <105049157+massifben@users.noreply.github.com>

--- sct-commons/src/main/java/org/lfenergy/compas/sct/commons/util/PrivateEnum.java
+++ sct-commons/src/main/java/org/lfenergy/compas/sct/commons/util/PrivateEnum.java
@@ -26,7 +26,8 @@ public enum PrivateEnum {
     COMPAS_ICDHEADER("COMPAS-ICDHeader", TCompasICDHeader.class),
     COMPAS_LDEVICE("COMPAS-LDevice", TCompasLDevice.class),
     COMPAS_SCL_FILE_TYPE("COMPAS-SclFileType", TCompasSclFileType.class),
-    COMPAS_SYSTEM_VERSION("COMPAS-SystemVersion", TCompasSystemVersion.class);
+    COMPAS_SYSTEM_VERSION("COMPAS-SystemVersion", TCompasSystemVersion.class),
+    COMPAS_TOPO("COMPAS-Topo", TCompasTopo.class);
 
     private static final Map<Class<?>, PrivateEnum> classToEnum = Arrays.stream(PrivateEnum.values()).collect(Collectors.toMap(
         compasPrivateEnum -> compasPrivateEnum.compasClass,
